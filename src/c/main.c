1a99151bbfc72f198a653b93f92f731ede64ca56
Morris <MorrisTimm@users.noreply.github.com>
2016-10-17 14:46:01 +0200

    tweaked the font and prepared color config

--- src/c/main.c
+++ src/c/main.c
@@ -7,6 +7,16 @@
 #define bw_bitmap_data_set_pixel(BMP, BPR, X, Y) (*((BMP)+(Y)*(BPR)+(X)/8)) |= (1 << (X)%8)
 #endif
 
+typedef struct {
+  GColor background_top;
+  GColor background_bottom;
+  GColor text_top;
+  GColor text_bottom;
+  GColor the_cut;
+} ColorSettings;
+
+static ColorSettings color_settings;
+
 static Window* my_window;
 
 static FFont* the_font;
@@ -49,24 +59,33 @@ static void tick_handler(struct tm* tick_time, TimeUnits units_changed) {
 
 static void top_layer_update_proc(Layer* layer, GContext* ctx) {
   GRect bounds = layer_get_bounds(layer);
+
+  // draw the background
+  graphics_context_set_fill_color(ctx, color_settings.background_top);
+  graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w, copy_window_start+copy_window_height), 0, 0);
+
+  // draw the text
   fctx_init_context(&fctx, ctx);
   fctx_begin_fill(&fctx);
-  fctx_set_text_em_height(&fctx, the_font, 100);
-  fctx_set_fill_color(&fctx, GColorWhite);
+  fctx_set_text_em_height(&fctx, the_font, 105);
+  fctx_set_fill_color(&fctx, color_settings.text_top);
   fctx_set_pivot(&fctx, FPointZero);
 #ifdef PBL_ROUND  
-  fctx_set_offset(&fctx, FPointI(bounds.size.w/2-8,bounds.size.h/3+12));
+  //fctx_set_offset(&fctx, FPointI(bounds.size.w/2-8,bounds.size.h/3+12));
+  fctx_set_offset(&fctx, FPointI(12, bounds.size.h/2+38));
 #else
-  fctx_set_offset(&fctx, FPointI(bounds.size.w/2-10,bounds.size.h/3+10));
+  //fctx_set_offset(&fctx, FPointI(bounds.size.w/2-10,bounds.size.h/3+10));
+  fctx_set_offset(&fctx, FPointI(0, bounds.size.h/2+38));
 #endif
   fctx_set_rotation(&fctx, 0);
-  fctx_draw_string(&fctx, top_text, the_font, GTextAlignmentCenter, FTextAnchorCapMiddle);
+  fctx_draw_string(&fctx, top_text, the_font, GTextAlignmentLeft, FTextAnchorBottom);
   fctx_end_fill(&fctx);
   fctx_deinit_context(&fctx);
 }
 
 static void top_cutting_layer_update_proc(Layer* layer, GContext* ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  // cut off the bottom by painting over it in the background color
+  graphics_context_set_fill_color(ctx, color_settings.background_bottom);
   graphics_context_set_antialiased(ctx, false);
   gpath_draw_filled(ctx, top_cutting_path);
 }
@@ -74,10 +93,12 @@ static void top_cutting_layer_update_proc(Layer* layer, GContext* ctx) {
 static void top_copy_layer_update_proc(Layer* layer, GContext* ctx) {
   GBitmap* buffer = graphics_capture_frame_buffer(ctx);
   GSize buffer_size = gbitmap_get_bounds(buffer).size;
+  // create backbuffer bitmap only once
   if(!copy_bitmap) {
     copy_bitmap = gbitmap_create_blank(GSize(buffer_size.w, copy_window_height), PBL_IF_COLOR_ELSE(GBitmapFormat8Bit, GBitmapFormat1Bit));
     bitmap_layer_set_bitmap(bitmap_layer, copy_bitmap);
   }
+  // copy the center window from the framebuffer into the backbuffer bitmap
   for(int i = copy_window_start; i < copy_window_start+copy_window_height; ++i) {
 #ifdef PBL_COLOR
     GBitmapDataRowInfo from_row_info = gbitmap_get_data_row_info(buffer, i);
@@ -91,31 +112,37 @@ static void top_copy_layer_update_proc(Layer* layer, GContext* ctx) {
 #endif
   }
   graphics_release_frame_buffer(ctx, buffer);
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(0, copy_window_start, buffer_size.w, copy_window_height), 0, 0);
-  layer_mark_dirty(bitmap_layer_get_layer(bitmap_layer));
 }
 
 static void bottom_layer_update_proc(Layer* layer, GContext* ctx) {
   GRect bounds = layer_get_bounds(layer);
+
+  // draw the background
+  graphics_context_set_fill_color(ctx, color_settings.background_bottom);
+  graphics_fill_rect(ctx, GRect(0, copy_window_start, bounds.size.w, bounds.size.h-copy_window_start), 0, 0);
+
+  // draw the text
   fctx_init_context(&fctx, ctx);
   fctx_begin_fill(&fctx);
-  fctx_set_text_em_height(&fctx, the_font, 100);
-  fctx_set_fill_color(&fctx, GColorPictonBlue);
+  fctx_set_text_em_height(&fctx, the_font, 105);
+  fctx_set_fill_color(&fctx, color_settings.text_bottom);
   fctx_set_pivot(&fctx, FPointZero);
 #ifdef PBL_ROUND  
-  fctx_set_offset(&fctx, FPointI(bounds.size.w/2+16,(bounds.size.h/3*2)-6));
+  //fctx_set_offset(&fctx, FPointI(bounds.size.w/2+16,(bounds.size.h/3*2)-6));
+  fctx_set_offset(&fctx, FPointI(bounds.size.w-12,bounds.size.h/2-12));
 #else
-  fctx_set_offset(&fctx, FPointI(bounds.size.w/2+10,(bounds.size.h/3*2)-2));
+  //fctx_set_offset(&fctx, FPointI(bounds.size.w/2+10,(bounds.size.h/3*2)-2));
+  fctx_set_offset(&fctx, FPointI(bounds.size.w,bounds.size.h/2-12));
 #endif
   fctx_set_rotation(&fctx, 0);
-  fctx_draw_string(&fctx, bottom_text, the_font, GTextAlignmentCenter, FTextAnchorCapMiddle);
+  fctx_draw_string(&fctx, bottom_text, the_font, GTextAlignmentRight, FTextAnchorCapTop);
   fctx_end_fill(&fctx);
   fctx_deinit_context(&fctx);
 }
 
 static void bottom_cutting_layer_update_proc(Layer* layer, GContext* ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  // cut off the top by painting over it in the background color
+  graphics_context_set_fill_color(ctx, color_settings.background_top);
   graphics_context_set_antialiased(ctx, false);
   gpath_draw_filled(ctx, bottom_cutting_path);
 }
@@ -124,13 +151,15 @@ static void bottom_copy_layer_update_proc(Layer* layer, GContext* ctx) {
   GBitmap* buffer = graphics_capture_frame_buffer(ctx);
   GSize buffer_size = gbitmap_get_bounds(buffer).size;
 
+  // copy from the center window in the framebuffer into the backbuffer bitmap
   for(int i = copy_window_start; i < copy_window_start+copy_window_height; ++i) {
 #ifdef PBL_COLOR
     GBitmapDataRowInfo from_row_info = gbitmap_get_data_row_info(buffer, i);
     GBitmapDataRowInfo to_row_info = gbitmap_get_data_row_info(copy_bitmap, i-copy_window_start);
     for(int j = from_row_info.min_x; j < from_row_info.max_x; ++j) {
-      if(   GColorBlack.argb != from_row_info.data[j]
-         && GColorBlack.argb == to_row_info.data[j]) {
+      // only copy foreground pixels to leave the pixels from the top layer
+      if(   color_settings.background_top.argb != from_row_info.data[j]
+         && color_settings.background_bottom.argb == to_row_info.data[j]) {
         to_row_info.data[j] = from_row_info.data[j];
       }
     }
@@ -140,6 +169,7 @@ static void bottom_copy_layer_update_proc(Layer* layer, GContext* ctx) {
     uint16_t copy_bytes_per_row = gbitmap_get_bytes_per_row(copy_bitmap);
     uint8_t* copy_bytes = gbitmap_get_data(copy_bitmap);
     for(int j = 0; j < buffer_size.w; ++j) {
+      // only copy foreground pixels to leave the pixels from the top layer
       if(   bw_bitmap_data_get_value(bytes, bytes_per_row, j, i)
         && !bw_bitmap_data_get_value(copy_bytes, copy_bytes_per_row, j, i-copy_window_start)) {
           bw_bitmap_data_set_pixel(copy_bytes, copy_bytes_per_row, j, i-copy_window_start);
@@ -148,35 +178,49 @@ static void bottom_copy_layer_update_proc(Layer* layer, GContext* ctx) {
 #endif
   }
   graphics_release_frame_buffer(ctx, buffer);
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(0, copy_window_start, buffer_size.w, copy_window_height), 0, 0);
   layer_mark_dirty(bitmap_layer_get_layer(bitmap_layer));
 }
 
 static void cut_layer_update_proc(Layer* layer, GContext* ctx) {
-  graphics_context_set_stroke_width(ctx, 5);
-  graphics_context_set_stroke_color(ctx, GColorBlack);
-  graphics_draw_line(ctx, cut[0], cut[1]);
+  // draw the cuting line
+  fctx_init_context(&fctx, ctx);
+  fctx_begin_fill(&fctx);
+  fctx_set_fill_color(&fctx, color_settings.the_cut);
+  fctx_move_to(&fctx, FPointI(cut[0].x, cut[0].y-2));
+  fctx_line_to(&fctx, FPointI(cut[1].x, cut[1].y-2));
+  fctx_line_to(&fctx, FPointI(cut[1].x, cut[1].y+2));
+  fctx_line_to(&fctx, FPointI(cut[0].x, cut[0].y+2));
+  fctx_line_to(&fctx, FPointI(cut[0].x, cut[0].y-2));
+  fctx_end_fill(&fctx);
+  fctx_deinit_context(&fctx);
   
-#if 0 // center line to help with adjustment
+#if 0 // center lines to help with adjustment
   GRect bounds = layer_get_bounds(layer);
   graphics_context_set_stroke_width(ctx, 1);
   graphics_context_set_stroke_color(ctx, GColorYellow);
   graphics_draw_line(ctx, GPoint(0, bounds.size.h/2), GPoint(bounds.size.w, bounds.size.h/2));
+  graphics_draw_line(ctx, GPoint(bounds.size.w/2, 0), GPoint(bounds.size.w/2, bounds.size.h));
 #endif
 }
 
 static void my_window_load(Window *window) {
+  // color config
+  color_settings.background_top = GColorBlack;
+  color_settings.background_bottom = GColorBlack;
+  color_settings.text_top = GColorWhite;
+  color_settings.text_bottom = GColorPictonBlue;
+  color_settings.the_cut = GColorBlack;
   
-  window_set_background_color(window, GColorBlack);
   Layer* root_layer = window_get_root_layer(window);
   GRect root_bounds = layer_get_bounds(root_layer);
 
   the_font = ffont_create_from_resource(RESOURCE_ID_THE_FONT);
   
+  // the endpoints of the cutting line
   cut[0] = GPoint(0, root_bounds.size.h/2+10);
-  cut[1] = GPoint(root_bounds.size.w-1, root_bounds.size.h/2-10);
+  cut[1] = GPoint(root_bounds.size.w, root_bounds.size.h/2-10);
 
+  // this can be fine tuned to save more RAM, it only needs to cover the area that overlaps
   copy_window_start = root_bounds.size.h/3;
   copy_window_height = root_bounds.size.h/3;
 
@@ -186,7 +230,7 @@ static void my_window_load(Window *window) {
 
   top_cutting_path_info.points[0] = cut[0];
   top_cutting_path_info.points[1] = cut[1];
-  top_cutting_path_info.points[2] = GPoint(root_bounds.size.w-1, copy_window_start+copy_window_height);
+  top_cutting_path_info.points[2] = GPoint(root_bounds.size.w, copy_window_start+copy_window_height);
   top_cutting_path_info.points[3] = GPoint(0, copy_window_start+copy_window_height);
   top_cutting_path = gpath_create(&top_cutting_path_info);
   
@@ -204,7 +248,7 @@ static void my_window_load(Window *window) {
 
   bottom_cutting_path_info.points[0] = cut[0];
   bottom_cutting_path_info.points[1] = cut[1];
-  bottom_cutting_path_info.points[2] = GPoint(root_bounds.size.w-1, copy_window_start);
+  bottom_cutting_path_info.points[2] = GPoint(root_bounds.size.w, copy_window_start);
   bottom_cutting_path_info.points[3] = GPoint(0, copy_window_start);
   bottom_cutting_path = gpath_create(&bottom_cutting_path_info);
   
